67f715dc0f61aa0c59c2babc4bb9789ea50d0f72
thonglee215 <87985272+thonglee215@users.noreply.github.com>
2023-02-10 17:26:24 +0700

    Done

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 var perDay = 100000;
 console.log(perDay);
 function tongLuong() {
-    var day = Number(document.getElementById("soNgay").value);
+    var day = document.getElementById("soNgay").value;
     var total = perDay * day
     document.getElementById("ketQua1").innerHTML = "Tiền lương của nhân viên là:  " + total.toLocaleString() + " VNĐ";
 }
@@ -53,3 +53,43 @@ function exchange() {
     document.getElementById("ketQua3").innerHTML = "Số tiền VNĐ sau khi quy đổi là:  " + total.toLocaleString() + " VNĐ";
 }
 document.getElementById("calc3").onclick = exchange;
+
+/** BÀI 4
+ * B1: Tạo Ui
+ * B2: Lấy dữ liệu từ input
+ * B3: Lập công thức
+ *       Diện tích HCN: Dài * Rộng
+ *       Chu vi HCN: (Dài + rộng) * 2
+ * B4: Thông báo output
+ */
+
+function SC() {
+    var length= Number(document.getElementById("cd").value);
+    var width= Number(document.getElementById("cr").value);
+    var S= length * width;
+    var C = (length + width) * 2;
+    document.getElementById("ketQua4").innerHTML = "Diện tích hình chữ nhât là:  " + S + "<br> Chu vi hình chữ nhật là:  " + C;
+}
+document.getElementById("calc4").onclick = SC;
+
+/** BÀI 5
+ * B1: Tạo Ui
+ * B2: Lấy dữ liệu từ input
+ * B3: Lập công thức
+ *       Tách phần chục: số / 10 (Lấy phần nguyên)
+ *       Tách phần đơn vị: số % 10 (Lấy phần nguyên)
+ * B4: Thông báo output
+ */
+
+function sum() {
+    var num= document.getElementById("so").value
+    if (num >= 10 && num <=99) {
+       var hangChuc = Math.floor(num / 10); 
+       var hangDv = Math.floor(num % 10); 
+       var tong= hangChuc + hangDv
+       document.getElementById("ketQua5").innerHTML = " Tổng của 2 ký số là:  " + tong;
+    } else {
+        alert("Nhập số có 2 chữ số nghe ba") ;
+    }
+}
+document.getElementById("calc5").onclick = sum;
